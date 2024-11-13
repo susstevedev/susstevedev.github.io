bb1e00cc0cd2f71672c274bd94c22aedb1204484
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-11-13 16:06:28 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.write('<style>
-        .twitter-pfp {
-          width: 100px;
-          height: 100px;
-          border-radius: 50%;
-          background-repeat: no-repeat;
-          background-size: cover;
-          background-image: url("https://pbs.twimg.com/profile_images/1856461944900694016/ZeqnUDy6_400x400.jpg");
-        }
-      </style>');        
+  document.write("<style> .twitter-pfp { width: 100px;height: 100px;border-radius: 50%;background-repeat: no-repeat;background-size: cover;background-image: url('https://pbs.twimg.com/profile_images/1856461944900694016/ZeqnUDy6_400x400.jpg'); } </style>");        
         
           $(document).ready(function() {
             function load(url) {
@@ -37,30 +28,5 @@ document.addEventListener("DOMContentLoaded", function() {
         		});
           });
         
-      document.write('<nav class="navbar" role="navigation" aria-label="main navigation">
-
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a href="/" class="navbar-item is-rounded">
-              Home
-            </a>
-
-            <a href="/about" class="navbar-item is-rounded">
-              About
-            </a>
-
-            <a href="/projects/" class="navbar-item is-rounded">
-              Projects
-            </a>
-      
-            <a href="https://twitter.com/Evan85908317" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">
-              Twitter
-            </a>
-
-            <a href="http://www.gr8brik.rf.gd/@Admin" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">
-              GR8BRIK
-            </a>
-            </div>
-          </div>
-      </nav>');
+      document.write('<nav class="navbar" role="navigation" aria-label="main navigation"><div id="navbarBasicExample" class="navbar-menu"><div class="navbar-start"><a href="/" class="navbar-item is-rounded">Home</a><a href="/about" class="navbar-item is-rounded">About</a><a href="/projects/" class="navbar-item is-rounded">Projects</a><a href="https://twitter.com/Evan85908317" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">Twitter</a><a href="http://www.gr8brik.rf.gd/@Admin" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">GR8BRIK</a></div></div></nav>');
   });
