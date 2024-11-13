eeb925c2beecf30592da229ba074d134d82d6a6c
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-11-13 16:19:21 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,6 @@
             function load(url) {
                 document.title = "";
                 $("body").load(url, function(event) {
-                     nav();
                     window.scrollTo(0, 0);
                     history.pushState(null, "", url);
                     document.title = $('title').text();
@@ -33,3 +32,4 @@ function nav() {
 
       document.write('<nav class="navbar" role="navigation" aria-label="main navigation"><div id="navbarBasicExample" class="navbar-menu"><div class="navbar-start"><a href="/" class="navbar-item is-rounded">&nbsp;Home&nbsp;</a><a href="/about" class="navbar-item is-rounded">&nbsp;About&nbsp;</a><a href="/projects/" class="navbar-item is-rounded">Projects</a><a href="https://twitter.com/Evan85908317" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">&nbsp;Twitter&nbsp;</a><a href="http://www.gr8brik.rf.gd/@Admin" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">&nbsp;GR8BRIK&nbsp;</a></div></div></nav>');
 }
+nav();
