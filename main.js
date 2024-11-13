1b60bde57c84505ec04b79a233dad241db71cf92
evanrutledge1 <168851101+evanrutledge1@users.noreply.github.com>
2024-11-13 16:14:54 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
    
         document.addEventListener("DOMContentLoaded", function() {
           $(document).ready(function() {
+            nav();
             function load(url) {
                 document.title = "";
                 $("body").load(url, function(event) {
                     window.scrollTo(0, 0);
                     history.pushState(null, "", url);
                     document.title = $('title').text();
+                    nav();
                 });
             }
     
@@ -27,6 +29,8 @@
         		});
           });
          });
+function nav() {
       document.write("<style> .twitter-pfp { width: 100px;height: 100px;border-radius: 50%;background-repeat: no-repeat;background-size: cover;background-image: url('https://pbs.twimg.com/profile_images/1856461944900694016/ZeqnUDy6_400x400.jpg'); } </style>");  
 
       document.write('<nav class="navbar" role="navigation" aria-label="main navigation"><div id="navbarBasicExample" class="navbar-menu"><div class="navbar-start"><a href="/" class="navbar-item is-rounded">&nbsp;Home&nbsp;</a><a href="/about" class="navbar-item is-rounded">&nbsp;About&nbsp;</a><a href="/projects/" class="navbar-item is-rounded">Projects</a><a href="https://twitter.com/Evan85908317" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">&nbsp;Twitter&nbsp;</a><a href="http://www.gr8brik.rf.gd/@Admin" target="_blank" class="navbar-item has-background-primary has-text-black is-rounded">&nbsp;GR8BRIK&nbsp;</a></div></div></nav>');
+}
