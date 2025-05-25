bbafcc09a970ea47c13f8cb06bfda5c80d90afde
Evan <168851101+susstevedev@users.noreply.github.com>
2025-05-25 08:17:49 -0500

    Update mb.js

--- mb.js
+++ mb.js
@@ -1,15 +1,33 @@
-fetch('https://corsproxy.io/?url=https://mecabricks.weetpix.com/en/user/sse2cpu')
+function fetch_models(username) {
+  fetch(`https://corsproxy.io/?url=https://mecabricks.weetpix.com/en/user/${username}`)
+    .then(response => response.text())
+    .then(html => {
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+      const first = doc.querySelector('.gallery-item a') ? doc.querySelector('.gallery-item a').href : null;
+    
+      if (first) {
+        const embed = document.createElement('iframe');
+        embed.src = `https://mecabricks.weetpix.com/${navigator.language.split("-")[0]}/player/${first.split('/').pop()}`;
+        embed.width = '640';
+        embed.height = '480';
+        document.querySelector('.embed').appendChild(embed);
+      }
+    });
+}
+function fetch_pfp(username) {
+  fetch(`https://corsproxy.io/?url=https://mecabricks.com/en/user/${username}`)
   .then(response => response.text())
   .then(html => {
     const parser = new DOMParser();
     const doc = parser.parseFromString(html, 'text/html');
-    const first = doc.querySelector('.gallery-item a') ? doc.querySelector('.gallery-item a').href : null;
-  
-    if (first) {
-      const embed = document.createElement('iframe');
-      embed.src = `https://mecabricks.weetpix.com/${navigator.language.split("-")[0]}/player/${first.split('/').pop()}`;
-      embed.width = '640';
-      embed.height = '480';
-      document.querySelector('.embed').appendChild(embed);
+    const url = doc.querySelector('#card-avatar').style.backgroundImage.slice(5, -2) ? doc.querySelector('#card-avatar').style.backgroundImage : null;
+
+    if(url) {
+      document.querySelector('.twitter-pfp').style.backgroundImage = url;
     }
   });
+}
+
+fetch_pfp('sse2cpu');
+fetch_models('sse2cpu');
