8e1ea171a4ef8025983109493cb4818640f4fa61
Evan <168851101+susstevedev@users.noreply.github.com>
2025-05-26 11:29:27 -0500

    Update mb.js

--- mb.js
+++ mb.js
@@ -1,12 +1,17 @@
+let user_page;
+
 function fetch_user_page(username) {
-   return fetch(`https://corsproxy.io/?url=https://mecabricks.weetpix.com/en/user/${username}`)
-    .then(response => response.text());
+  if (!user_page) {
+    user_page = fetch(`https://corsproxy.io/?url=https://mecabricks.weetpix.com/en/user/${username}`)
+      .then(response => response.text());
+  }
+  return user_page;
 }
 
 function fetch_models(username) {
-  fetch_user_page(username).then(html => {
+  //fetch_user_page(username).then(html => {
      const parser = new DOMParser();
-     const doc = parser.parseFromString(html, 'text/html');
+     const doc = parser.parseFromString(user_page, 'text/html');
      const first = doc.querySelector('.gallery-item a') ? doc.querySelector('.gallery-item a').href : null;
        
      if (first) {
@@ -16,31 +21,31 @@ function fetch_models(username) {
        embed.height = '480';
        document.querySelector('.embed').appendChild(embed);
      }
-  });
+  //});
 }
 
 function fetch_bio(username) {
-   fetch_user_page(username).then(html => {
+   //fetch_user_page(username).then(html => {
       const parser = new DOMParser();
-      const doc = parser.parseFromString(html, 'text/html');
+      const doc = parser.parseFromString(user_page, 'text/html');
       const bio = doc.querySelector('#card-about') ? doc.querySelector('#card-about') : null;
     
       if (bio) {
          document.querySelector('.bio').innerText = bio.innerText;
       }
-   });
+   //});
 }
 
 function fetch_pfp(username) {
-  fetch_user_page(username).then(html => {
+  //fetch_user_page(username).then(html => {
     const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
+    const doc = parser.parseFromString(user_page, 'text/html');
     const url = doc.querySelector('#card-avatar').style.backgroundImage.slice(5, -2) ? doc.querySelector('#card-avatar').style.backgroundImage : null;
 
     if(url) {
       document.querySelector('.twitter-pfp').style.backgroundImage = url;
     }
-  });
+  //});
 }
 
 fetch_pfp('sse2cpu');
