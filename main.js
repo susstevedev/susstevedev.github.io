fae2fc15392d389e5f52068b56fb3a1413cc1b2e
Evan <168851101+susstevedev@users.noreply.github.com>
2025-11-04 10:11:43 -0600

    B64 backups of bio

--- main.js
+++ main.js
@@ -24,4 +24,5 @@ document.addEventListener("DOMContentLoaded", function() {
    });
 });
 
-let bio_legacy = "SGkgSSBkbyB3ZWIgZGV2ZWxvcG1lbnQuIEkgYWxzbyBsaWtlIExlZ28uIEplc3VzIGFuZCAob2xkIGVyYSkgS2FueWUgZ2xhemllci4gQWxzbyBJJ20gYmlzZXh1YWw=";
+let bio_legacy_b64 = "SGkgSSBkbyB3ZWIgZGV2ZWxvcG1lbnQuIEkgYWxzbyBsaWtlIExlZ28uIEplc3VzIGFuZCAob2xkIGVyYSkgS2FueWUgZ2xhemllci4gQWxzbyBJJ20gYmlzZXh1YWw=";
+let current_bio_b64 = "SGkgSSdtIDE0IChoZS9oaW0pIGFuZCBkbyB3ZWIgZGV2ZWxvcG1lbnQgYW5kIExlZ28gc3R1ZmYuIENocmlzdGlhbi4gQmkgYW5kIHZlcnNhdGlsZS4gSSdtIHNlY29uZCB0byBoYXZpbmcgdGhlIG1vc3QgZm9ydW0gcG9zdHMuIEFJLCBNQUdBLCBwM2RvLCBpbmMzc3QgYW5kIHJAcGUgc3VwcG9ydGVycyBETkku";
