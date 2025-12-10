fc8ac7593845830c35180db51e307977f7882531
susstevedev <168851101+susstevedev@users.noreply.github.com>
2025-12-10 14:51:43 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
+(function() {
+//document.addEventListener("DOMContentLoaded", function() {
    $(document).ready(function() {
       $(document).on('click', 'a', function(event) {
          var url = $(this).attr("href");
@@ -30,7 +31,8 @@ document.addEventListener("DOMContentLoaded", function() {
    });
 
    document.querySelector('.bio').innerText = "Hi I'm 14 (he/him, bisexual) and do web development and Lego stuff. Also Christian. Creator of Gr8Brik.";
-});
+//});
+})();
 
 let bio_legacy_b64 = "SGkgSSBkbyB3ZWIgZGV2ZWxvcG1lbnQuIEkgYWxzbyBsaWtlIExlZ28uIEplc3VzIGFuZCAob2xkIGVyYSkgS2FueWUgZ2xhemllci4gQWxzbyBJJ20gYmlzZXh1YWw=";
 let current_bio_b64 = "SGkgSSdtIDE0IChoZS9oaW0sIGJpKSBhbmQgZG8gd2ViIGRldmVsb3BtZW50IGFuZCBMZWdvIHN0dWZmLiBDaHJpc3RpYW4uIFN1cnZpdm9yIG9mIGxvdHMgb2YgTWVjYWJyaWNrcyBnZW5lcmF0aW9ucy4gTWVtYmVyIG9mIHRoZSBHcjhCcmlrIGFkbWluIGFuZCBkZXZlbG9wbWVudCB0ZWFtLiBJIGhhdmUgdGhlIG1vc3QgZm9ydW0gcG9zdHMuIEFJLCBNQUdBLCBxdWVlcnBob2JpYSwgcDNkbywgaW5jM3N0IGFuZCByQHBlIHN1cHBvcnRlcnMgRE5JLg==";
